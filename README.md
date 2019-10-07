dfba3aee9d8b42254c36e38e7f881ccc341b004a
KirillGaydamaka <k.gaydamaka@gmail.com>
2019-10-07 09:46:52 +0300

    Исходная работающая версия

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
 # Anti-Duplicator
 
-[TODO. There will be project description]
+This script allows to find duplicate files in folder and it's subfolders.
+You need to specify folder path as a parameter. 
 
+# Quickstart
+
+The script needs Python 3.5 interpreter.
+
+Example of script launch on Linux, Python 3.5:
+
+```bash
+$ python duplicates.py <path to folder>
+```
+
+Example of script output:
+
+```bash
+Duplicate files:
+test.py of size 109 bytes
+3.txt of size 0 bytes
+
+Process finished with exit code 0
+```
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,36 @@
+import os
+import argparse
+
+def create_parser():
+    parser = argparse.ArgumentParser(description='Parameters')
+    parser.add_argument('path', help='Path to folder')
+    args = parser.parse_args()
+    return args
+
+def get_file_sizes(path):
+    file_sizes_list = []
+    for root, dirs, files in os.walk(path):
+        for file_name in files:
+            file_path = os.path.join(root, file_name)
+            file_size = os.path.getsize(file_path)
+            file_sizes_list.append((file_name, file_size))
+    return file_sizes_list
+
+def main():
+
+    try:
+        path = create_parser().path
+        if not os.path.exists(path):
+            print('Folder does not exist')
+            return None
+        file_sizes_list = get_file_sizes(path)
+        duplicates_set = set([x for x in file_sizes_list if file_sizes_list.count(x)>1])
+        print('Duplicate files:')
+        for duplicate in duplicates_set:
+            print(duplicate[0], 'of size', duplicate[1], 'bytes')
+    except OSError:
+        print("Can't read files")
+
 
 if __name__ == '__main__':
-    pass
+    main()
